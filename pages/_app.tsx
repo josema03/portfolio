46cc63daf7d33a75bd97a7fcd2dbd1af52ffbafa
José Marín <74282573+josema03@users.noreply.github.com>
2021-08-08 14:05:36 -0400

    feat: initial setup and SSR with styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
 }
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import NextDocument, { DocumentContext } from "next/document";
+import React from "react";
+import { ServerStyleSheet as StyledComponentSheets } from "styled-components";
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext) {
+    const styledComponentSheet = new StyledComponentSheets();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            styledComponentSheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await NextDocument.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <React.Fragment key="styles">
+            {initialProps.styles}
+            {styledComponentSheet.getStyleElement()}
+          </React.Fragment>,
+        ],
+      };
+    } finally {
+      styledComponentSheet.seal();
+    }
+  }
+}
